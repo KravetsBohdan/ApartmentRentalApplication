665d742890b423bdb5a2d1a6ebe187b796dd0bd0
KravetsBohdan <bodyakravets@gmail.com>
2023-03-25 14:31:23 +0200

    add @OpenAPIDefinition

--- src/main/java/com/bkravets/apartmentrentalapp/ApartmentRentalAppApplication.java
+++ src/main/java/com/bkravets/apartmentrentalapp/ApartmentRentalAppApplication.java
@@ -1,11 +1,16 @@
 package com.bkravets.apartmentrentalapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+        servers = @Server(url = "/")
+)
 public class ApartmentRentalAppApplication {
 
     public static void main(String[] args) {
