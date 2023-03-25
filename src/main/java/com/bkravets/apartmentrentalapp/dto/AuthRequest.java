c92d94a95b6f537fd0d34d07940b06b727e373dd
KravetsBohdan <bodyakravets@gmail.com>
2023-03-25 13:31:26 +0200

    add validation

--- src/main/java/com/bkravets/apartmentrentalapp/dto/AuthRequest.java
+++ src/main/java/com/bkravets/apartmentrentalapp/dto/AuthRequest.java
@@ -1,5 +1,8 @@
 package com.bkravets.apartmentrentalapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthRequest {
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Size(min = 8, message = "Password should be at least 8 characters")
     private String password;
 }
