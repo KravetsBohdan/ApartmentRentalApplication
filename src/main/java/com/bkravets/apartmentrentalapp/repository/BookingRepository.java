ad92fbf65ff6712877fa6cca76f81f937aa40f1e
KravetsBohdan <bodyakravets@gmail.com>
2023-03-28 10:20:34 +0300

    clean up

--- src/main/java/com/bkravets/apartmentrentalapp/repository/BookingRepository.java
+++ src/main/java/com/bkravets/apartmentrentalapp/repository/BookingRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findAllByApartmentId(long apartmentId);
-
-    Optional<Booking> findByTenantIdAndApartmentId(long id, long apartmentId);
 
     List<Booking> findAllByEndDateBeforeOrStatus(LocalDate date, Status status);
 }
